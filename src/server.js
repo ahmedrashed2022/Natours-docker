25802c7741f9907a107cb38171278a18e93a54a4
ahmedrashed2022 <ahmed.rashed12323@gmail.com>
2023-05-17 21:23:19 +0300

    Final Commit

--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const redis = require('redis');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
+//connect to redis
+const REDIS_PORT = 6379;
+const REDIS_HOST = 'redis';
+
+const redisClient = redis.createClient({
+  url: `redis://${REDIS_HOST}:${REDIS_PORT}`
+});
+redisClient.on('error', err => console.log('redis client error:', err));
+redisClient.on('connect', () => console.log('Connected to redis'));
+redisClient.connect();
 //connect db
 
 const DB_USER = 'root';
